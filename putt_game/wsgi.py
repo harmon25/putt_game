357bdac86f47f3e3199dd4036cef0af17725573c
harmon25 <dougwright1@gmail.com>
2014-08-14 17:35:40 -0700

    start of project

---
+++ putt_game/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for putt_game project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "putt_game.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
